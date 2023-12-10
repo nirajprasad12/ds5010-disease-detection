db74aed85397f70d475f6b85ef9fb20dbeaa037d
nirajprasad12 <nirajsaiprasad@gmail.com>
2023-12-10 16:57:32 -0500

    adding libraries back to diabetes.py

--- disease_detection/diabetes.py
+++ disease_detection/diabetes.py
@@ -1,3 +1,25 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+import os
+import pkg_resources
+
 resource_package = __name__
 resource_path = '/'.join(('datasets', 'diabetes.csv'))  
 template = pkg_resources.resource_stream(resource_package, resource_path)
