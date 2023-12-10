9c7b189cd5c930e0883f380ef560f4e6d49103eb
nirajprasad12 <nirajsaiprasad@gmail.com>
2023-12-10 16:56:36 -0500

    making changes to init file

--- disease_detection/__init__.py
+++ disease_detection/__init__.py
@@ -1,23 +1,3 @@
 # Library import
 
-import warnings
-warnings.filterwarnings("ignore")
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-
-import os
-import pkg_resources
