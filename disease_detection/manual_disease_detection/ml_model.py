299a53b77a07f18e4f296609f3e251c5fad32962
nirajprasad12 <nirajsaiprasad@gmail.com>
2023-12-10 20:00:18 -0500

    adding new features to ml_model.py

--- disease_detection/manual_disease_detection/ml_model.py
+++ disease_detection/manual_disease_detection/ml_model.py
@@ -1,5 +1,6 @@
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,19 +8,34 @@ from sklearn.metrics import confusion_matrix
 
 def train_random_forest(X_train, y_train, n_estimators=100, max_depth=None):
     # Train Random Forest classifier
-    clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-    clf.fit(X_train, y_train)
-    return clf
+    clf_rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    clf_rf.fit(X_train, y_train)
+    return clf_rf
+    
+def train_logistic_regression(X_train, y_train, random_state = 0):
+    classifier_lr = LogisticRegression(random_state = random_state)
+    classifier_lr.fit(X_train, y_train)
+    return classifier_lr
 
-def evaluate_model(clf, X_test, y_test):
+def train_k_nearest_neighbours(X_train, y_train, n_neighbors = 5, metric = 'minkowski', p = 2):
+    classifier_knn = KNeighborsClassifier(n_neighbors = n_neighbors, metric = metric, p = p)
+    classifier_knn.fit(X_train, y_train)
+    return classifier_knn
+
+def evaluate_model(clf, X_test, y_test, req_result = 0):
     # Make predictions
     y_pred = clf.predict(X_test)
-
     # Evaluate the model
     accuracy = accuracy_score(y_test, y_pred)
     report = classification_report(y_test, y_pred)
-
-    return y_pred, accuracy, report
+    if req_result == 0:
+        return y_pred, accuracy, report
+    elif req_result == 1:
+        return y_pred
+    elif req_result == 2:
+        return accuracy
+    elif req_result == 3:
+        return report
 
 def plot_confusion_matrix(y_true, y_pred):
     # Plot confusion matrix
